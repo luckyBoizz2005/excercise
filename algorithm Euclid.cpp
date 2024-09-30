316b0b30a572e441979fcc3037bf068d4cbbd18c
luckyBoizz2005 <23020786@vnu.edu.vn>
2024-09-30 20:03:58 +0700

    Add files via upload

---
+++ algorithm Euclid.cpp
@@ -0,0 +1,21 @@
+// #include <iostream>
+// #include <algorithm>
+// using namespace std;
+// void rutgon(int a, int b)
+// {
+//     int x = a, y = b;
+//     while (b != 0)
+//     {
+//         int tmp = b;
+//         b = a % b;
+//         a = tmp;
+//     }
+//     int ucln = a;
+//     cout << "rut gon la: " << x / ucln << "/" << y / ucln;
+// }
+// int main()
+// {
+//     int a, b;
+//     cin >> a >> b;
+//     rutgon(a, b);
+// }
